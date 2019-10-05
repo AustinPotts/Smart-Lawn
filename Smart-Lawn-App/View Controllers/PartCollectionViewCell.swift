d2c60eb2648cf54cdd4f25aa220e73b169d7d1c5
AustinPotts <pottsevolvements@gmail.com>
2019-10-05 12:45:49 -0400

    Configured Collection View + Outlets

---
+++ Smart-Lawn-App/View Controllers/PartCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PartCollectionViewCell.swift
+//  Smart-Lawn-App
+//
+//  Created by Austin Potts on 10/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PartCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var partImage: UIImageView!
+    @IBOutlet weak var partLabel: UILabel!
+    
+}
