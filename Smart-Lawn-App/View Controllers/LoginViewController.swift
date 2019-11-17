b1de739263e4183f4b1c085521a66396c4da9f81
AustinPotts <pottsevolvements@gmail.com>
2019-11-16 20:55:39 -0500

    Re Configured Login Layout
    Changing projects layout for more modern scheme.

---
+++ Smart-Lawn-App/View Controllers/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Smart-Lawn-App
+//
+//  Created by Austin Potts on 11/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+}
